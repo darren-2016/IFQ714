a376b84e8afc1c9f5da4a3c08c9191de54435b7f
Darren Draper <darren.j.draper@gmail.com>
2023-07-23 15:39:03 +1000

    Initial commit

---
+++ CodePlay1/codeplay1.js
@@ -0,0 +1,15 @@
+/* Code-play 1: Variables and types
+   Experimenting with JavaScript Objects.
+   In the TERMINAL window, use the following to execute the script:  node codeplay4.js 
+  */
+
+   // Declaration of variables
+let hello;
+hello = "Hello";
+console.log (hello);
+
+let five = 5;
+console.log(five);
+
+const inUse = true;
+console.log(inUse);
