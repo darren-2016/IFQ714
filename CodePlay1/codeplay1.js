29c81001b23d797d4f7c18f27fbe0ce69e141de2
Darren Draper <darren.j.draper@gmail.com>
2023-07-23 15:53:55 +1000

    Updated with rest of code play items, i.e. Operators (Numbers, Strings, Booleans), and Errors with Variables

--- CodePlay1/codeplay1.js
+++ CodePlay1/codeplay1.js
@@ -3,7 +3,7 @@
    In the TERMINAL window, use the following to execute the script:  node codeplay4.js 
   */
 
-   // Declaration of variables
+// Declaration of variables
 let hello;
 hello = "Hello";
 console.log (hello);
@@ -13,3 +13,49 @@ console.log(five);
 
 const inUse = true;
 console.log(inUse);
+
+// Using operators
+// 1. Numbers
+console.log(5 + 4);
+console.log(5 - 4);
+console.log(5 * 4);
+console.log(5 / 4);
+console.log(5 % 4);
+
+console.log(5 + 4 - 3);
+console.log(5 + 4 * 3);
+
+console.log((5 + 4) - 3);
+console.log((5 + 4) * 3);
+
+console.log((five + 4) - 3);
+console.log((five + 4) * 3);
+
+// 2. Strings
+console.log("Hello" + "World");
+console.log("Hello" + " " + "World" + "!");
+console.log("Hello" + 5);
+console.log(hello + "world");
+console.log("This and\\or that");
+console.log("This is the first line,\nthat on the second line");
+
+// 3. Booleans
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+
+console.log(9 ==5);
+console.log(9 != 5);
+console.log(9 > 5);
+console.log(9 <= 5);
+
+console.log("9" == 9);
+console.log("9" === 9);
+
+console.log(five == 5);
+
+// Errors with variables - uncomment to see the errors in the node interpreter
+//let five = "five";
+//inUse = false;
+console.log("hello" - 5);
+console.log("5" - 5);
