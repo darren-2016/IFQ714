bb2f8c198a42a431ff4aed5e71ee2b2151a00533
Darren Draper <darren.j.draper@gmail.com>
2023-07-25 21:48:54 +1000

    Tidy up code and add further comments

--- Module2Workshop/module2.js
+++ Module2Workshop/module2.js
@@ -3,26 +3,36 @@
    The data is exported by their system into JSON format.
 */
 
+// Step 1
 // Create some employee and sale objects
-const employee1 = JSON.parse('{"id":1,"firstName":"John", "lastName":"Smith", "gender":"Male", "age":23, "position":"Manager"}');
+const employee1 = JSON.parse('{"id":1, "firstName":"John", "lastName":"Smith", "gender":"Male", "age":23, "position":"Manager"}');
 console.log(employee1);
 
 const sale1 = JSON.parse('{"staffId":1, "item":"Wi-Fi Adapter", "price":40.00, "date":"01-09-2022"}');
 console.log(sale1);
 
+// Step 2
 // Function to output formatted version of the employee info
 function showEmployeeInfo(employee) {
-    console.log(`Name: ${employee.firstName} ${employee.lastName}\nStaff ID: ${employee.id}\nGender: ${employee.gender}\nAge: ${employee.age}\nPosition: ${employee.position}`);
+    console.log(`Name: ${employee.firstName} ${employee.lastName}\n
+                 Staff ID: ${employee.id}\n
+                 Gender: ${employee.gender}\n
+                 Age: ${employee.age}\n
+                 Position: ${employee.position}`);
 }
 
 // Function to output formatted version of the sale info
 function showSaleInfo(sale) {
-    console.log(`Item: ${sale.item}\nPrice: ${sale.price}\nDate of sale: ${sale.date}\nID of Staff Member: ${sale.staffId}`);
+    console.log(`Item: ${sale.item}\n
+                 Price: ${sale.price}\n
+                 Date of sale: ${sale.date}\n
+                 ID of Staff Member: ${sale.staffId}`);
 }
+
 showEmployeeInfo(employee1);
 showSaleInfo(sale1);
 
-
+// Step 3
 // Constructor to define a new employee
 function Employee(id, firstName, lastName, gender, age, position) {
     this.id = id;
@@ -56,6 +66,7 @@ let employee2 = new Employee(2, "Mary", "Sue", "Female", 32, "Salesperson");
 // Create a new sale
 let sale2 = new Sale(1, "Wi-Fi Adapter", 40.00, "03-09-2022");
 
+// Step 4
 // Create arrays for the employee and sale objects
 let employees = [employee1, employee2];
 let sales = [sale1, sale2];
@@ -63,5 +74,8 @@ let sales = [sale1, sale2];
 console.log(employees);
 console.log(sales);
 
+
+
+// Bonus tasks
 employee2.showInfo();
 sale2.showInfo();
