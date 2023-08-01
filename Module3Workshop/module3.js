5b3acf246100853aac6af28b3930e08e02dd9c41
Darren Draper <darren.j.draper@gmail.com>
2023-08-01 17:22:18 +1000

    Create function to traverse all the employees and sales and show info for each.

--- Module3Workshop/module3.js
+++ Module3Workshop/module3.js
@@ -3,24 +3,58 @@
    The data is exported by their system into JSON format.
 */
 
+// Include the Node file system, to use for reading in data files
 const fs = require('fs');
 
-// Read Employee data from external file into internal object
+// Read Employee data from external file (JSON format) into internal object
 // Workshop_JSON_Employees.txt
 const data = fs.readFileSync('./Workshop_JSON_Employees.txt', { encoding: 'utf8', flag: 'r'});
 console.log(data);
-let Employee = JSON.parse(data);
-console.log(Employee);
+let Employees = JSON.parse(data);
+console.log(Employees);
 
 
-// Read Sales data from external file into internal object
+// Read Sales data from external file (JSON format) into internal object
 //Workshop_JSON_Sales.txt
 const salesData = fs.readFileSync('./Workshop_JSON_Sales.txt', { encoding: 'utf8', flag: 'r'});
-console.log(salesData);
+//console.log(salesData);
 let Sales = JSON.parse(salesData);
-console.log(Sales);
+//console.log(Sales);
+
+//let numberOfEmployees = Employees.length;
+//console.log(numberOfEmployees);
+
+//let numberOfSales = Sales.length;
+//console.log(numberOfSales);
+
+// Function to output formatted version of the employee info
+function showEmployeeInfo(employee) {
+   console.log(`Name: ${employee.firstName} ${employee.lastName}\nStaff ID: ${employee.id}\nGender: ${employee.gender}\nAge: ${employee.age}\nPosition: ${employee.position}`);
+}
+
+// Function to traverse all the employees and show information for each
+function showAllEmployeesInfo(employees) {
+   console.log("Show employees info");
+   for (let i = 0; i < employees.length; i++) {
+      showEmployeeInfo(employees[i]);
+   }
+}
+
+// Function to output formatted version of the sale info
+function showSaleInfo(sale) {
+   console.log(`Item: ${sale.item}\nPrice: ${sale.price}\nDate of sale: ${sale.date}\nID of Staff Member: ${sale.staffId}`);
+}
+
+// Function to traverse all the sales and show information for each
+function showAllSalesInfo(sales) {
+   console.log("Show sales info");
+   for (let i = 0; i < sales.length; i++) {
+      showSaleInfo(sales[i]);
+   }
+}
 
 
 
-//const jsonObj = JSON.stringify(myData);
-//console.log(jsonObj);
+showAllEmployeesInfo(Employees);
+
+showAllSalesInfo(Sales);
