ad9ed41868f149f49c9212ab704dd2361eb88e48
Darren Draper <darren.j.draper@gmail.com>
2023-08-07 09:14:41 +1000

    Experimenting with code and debugging

--- CodePLay12/codeplay12.js
+++ CodePLay12/codeplay12.js
@@ -5,11 +5,13 @@
 
 // Example code for testing objects
 
+
 // Objects to test with.
 const testObject1 = {name:"Flying Scotsman", class:"Gresley A3", power:"steam", date:1934, kmh:161};
 const testObject2 = {name:"M-497", class:"Modified Budd RDC-3", power:"jet", date:1966, kmh:295};
 
 // Function for comparing different train speed records.
+//function getFastestTrain(input1, input3) {
 function getFastestTrain(input1, input2) {
    console.log("Entering getFastestTrain function");
    console.log(`Comparing ${input1.name} with ${input2.name}`);
@@ -19,16 +21,17 @@ function getFastestTrain(input1, input2) {
    } else if (input1.kmh < input2.kmh) {
       console.log(`Returning the second train which is faster (${input1.kmh} < ${input2.kmh})`);
       return input2;
-   } else if (input1.kmh === input2.kmh) {
+   } else if (input1.kmh === input2.kmh && typeof input1.kmh !== "undefined") {
       console.log("Returning both trains since they have the same speed record");
       return [input1, input2];
    } else {
-      console.log("Returning null since we coudln;t make a comparison");
+      console.log("Returning null since we couldn't make a comparison");
       return null;
    }
 }
 
 // Call the function with the test values and show the result.
 //console.log(getFastestTrain(testObject1,testObject2));
-console.table(getFastestTrain(testObject1,testObject2));
+//console.table(getFastestTrain(testObject1,testObject2));
 //console.table(getFastestTrain(testObject1,testObject1));
+console.table(getFastestTrain(5,10));
