ae416e52e7ff992d4bc92c5d54333430afed7e1b
Darren Draper <darren.j.draper@gmail.com>
2023-08-07 16:11:44 +1000

    Initial commit

---
+++ CodePlay13/UnitTest/codeplay13.js
@@ -0,0 +1,14 @@
+// Example code for testing
+
+// Function for squaring numbers from a string, if it can be parsed
+function parseThenSquare(input) {
+    let num = Number.parseInt(input);
+    if (!Number.isNaN(num)) {
+        return num * num;
+    } else {
+        return null;
+    }
+}
+
+// Export the function as a module
+module.exports = parseThenSquare;
