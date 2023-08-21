89a7c399838c9848249eaa9d6be64e825fb3ddfb
Darren Draper <darren.j.draper@gmail.com>
2023-08-21 12:13:49 +1000

    Initial commit

---
+++ CodePlay15/codeplay15.js
@@ -0,0 +1,10 @@
+/**
+ * Code-play 15: Working with Arrays
+   Experimenting with arrays
+   In the TERMINAL window, use the following to execute the script:  node codeplay15.js 
+  */
+
+   let numbers = [200, 50, 1, 9001];
+   numbers.sort();
+   console.log(numbers);
+   
