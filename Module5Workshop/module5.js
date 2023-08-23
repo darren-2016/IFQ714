2ce982aba275059f6d101800b7687679dfd2fd61
Darren Draper <darren.j.draper@gmail.com>
2023-08-23 23:54:48 +1000

    Added greatestCommonDivisor() function

--- Module5Workshop/module5.js
+++ Module5Workshop/module5.js
@@ -216,4 +216,24 @@ salesCommission();
 console.log("********************************************");
 console.log("********************************************");
 
+/**
+ * Recursive function to calculate greatest common divisor
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+function greatestCommonDivisor(a, b) {
+   if( b === 0) {
+      return a;
+   } else {
+      return greatestCommonDivisor(b, a % b);
+   }
+}
+
+console.log(greatestCommonDivisor(14,4));
+
+
 module.exports = findEmployeeById;
+
+
+
