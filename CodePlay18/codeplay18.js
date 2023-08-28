ffbefff2788d1ac68f648198503a12f3f2eec7fb
Darren Draper <darren.j.draper@gmail.com>
2023-08-28 11:44:57 +1000

    Initial commit

---
+++ CodePlay18/codeplay18.js
@@ -0,0 +1,7 @@
+/**
+ * Code-play 18: Events and listeners
+   Experimenting with events and listeners
+   In the TERMINAL window, use the following to execute the script:  node codeplay18.js 
+  */
+
+   
