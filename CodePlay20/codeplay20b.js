24021bcc25c86cc22c25773ccf4d1c7341ce9b77
Darren Draper <darren.j.draper@gmail.com>
2023-08-29 13:56:41 +1000

    Initial commit

---
+++ CodePlay20/codeplay20b.js
@@ -0,0 +1,16 @@
+const newPromise = new Promise(function (resolve, reject) {
+    try {
+        let answer = 5 + 5;
+        resolve(answer);
+    } catch (error) {
+        reject(error);
+    }
+});
+
+function resolveCallback (result) { console.log(`Result is: ${result}`);}
+function rejectCallback (error) { console.log(`Error is: ${error.message}`);}
+
+newPromise.then(
+    resolveCallback,
+    rejectCallback
+);
