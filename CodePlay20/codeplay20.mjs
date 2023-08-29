da15835a176add2dd49ee051c5e5d03aeb93e4fe
Darren Draper <darren.j.draper@gmail.com>
2023-08-29 14:21:36 +1000

    async and await implementation

--- CodePlay20/codeplay20.mjs
+++ CodePlay20/codeplay20.mjs
@@ -1,8 +1,15 @@
 import fetch from "node-fetch";
 //fetch("https://www.w3.org/TR/PNG/iso_8859-1.txt")
-fetch("car")
+/*fetch("car")
     .then(function (response) { return response.text(); })
     .then(function (data) { console.log(data); })
     .catch(function (error) { console.log(`Error occured: ${error.message}`); });
+*/
 
+async function getUrl(url) {
+    let response = await fetch(url);
+    let text = await response.text();
+    console.log(text);
+}
 
+getUrl("https://www.w3.org/TR/PNG/iso_8859-1.txt");
