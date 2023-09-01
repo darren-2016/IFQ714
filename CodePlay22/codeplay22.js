eb4aad8d65fbf885f2a537ecfbc290db62ff3a1d
Darren Draper <darren.j.draper@gmail.com>
2023-09-01 14:59:29 +1000

    Stream file reading implementation

--- CodePlay22/codeplay22.js
+++ CodePlay22/codeplay22.js
@@ -12,3 +12,12 @@ fs.readFile("DataFiles/datafile.txt", "utf8", function (error, text) {
     else { console.log(text); }
 });
 
+// Stream file reading
+let stream = fs.createReadStream("DataFiles/datafile.txt", "utf8");
+
+stream.on("readable", function() {
+    let chunk = stream.read(100);
+    console.log(`Current chunk: ${chunk}`);
+});
+stream.on("end", function() { stream.close(); });
+stream.on("error", function(error) { console.log(error.message); stream.close(); });
