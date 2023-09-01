e6cf3ced8532e84c0af28772b3c7aa1ba65ab76f
Darren Draper <darren.j.draper@gmail.com>
2023-09-01 16:22:17 +1000

    Initial commit

---
+++ CodePlay23/codeplay23.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3');
+
+let database = new sqlite3.Database("Chinook_Sqlite.sqlite", function(error) {
+    if (error) {
+        console.log(`Failed to connect to Chinook database: ${error.message}`);
+    } else {
+        console.log("Connected to Chinook database");
+    }
+});
+
+database.close();
